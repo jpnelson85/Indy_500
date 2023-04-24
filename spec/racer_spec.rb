b42f2b4b2f00626213d8d8587fbc88e85dd8989a
Jeff Nelson <jpnelson85@gmail.com>
2023-04-24 08:52:12 -0500

    add to race field methods and tests

--- spec/racer_spec.rb
+++ spec/racer_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Racer do
   it 'exists' do
-    racer1 = Racer.new(name: "Tony Kanaan", country: "Brazil", age: 37)
+    racer1 = Racer.new({name: "Tony Kanaan", country: "Brazil", team: "Chip Ganassi Racing", age: 37, position: 5, number: 1, practice_top_speed: 232.037, pole_avg_speed: 230.098})
     expect(racer1).to be_a(Racer)
   end
 
   it 'has attributes' do
-    racer1 = Racer.new(name: "Tony Kanaan", country: "Brazil", team: "Chip Ganassi Racing" age: 37, position: 5, number: 1, practice_top_speed: 232.037, pole_avg_speed: 230.098)
+    racer1 = Racer.new({name: "Tony Kanaan", country: "Brazil", team: "Chip Ganassi Racing", age: 37, position: 5, number: 1, practice_top_speed: 232.037, pole_avg_speed: 230.098})
     expect(racer1.name).to eq("Tony Kanaan")
     expect(racer1.country).to eq("Brazil")
     expect(racer1.team).to eq("Chip Ganassi Racing")
