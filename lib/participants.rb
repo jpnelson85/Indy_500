544f5d3530c9e16874cb08cff98b0b0e09fc3444
Jeff Nelson <jpnelson85@gmail.com>
2023-04-24 09:29:38 -0500

    add tests and methods for participants class

--- lib/participants.rb
+++ lib/participants.rb
@@ -1,1 +1,11 @@
-class
+class Participants
+  attr_reader :list_of_participants
+
+  def initialize
+    @list_of_participants = []
+  end
+
+  def add_participant(participant)
+    @list_of_participants << participant
+  end
+end
